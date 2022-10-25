c7adcd2a09eab192a5735389abc8faabe89536b8
Anton Kanavalau <old.projecta@gmail.com>
2022-10-25 16:15:43 +0300

    Finish task_1

---
+++ lesson-2-basic-constructions/Task_1.js
@@ -0,0 +1,18 @@
+"use strict"
+
+var string = prompt('Введите строку');
+var vowels = ['а', 'о', 'у', 'э', 'ы', 'я', 'ё', 'е', 'ю', 'и'];
+var count = 0;
+
+amountVowels();
+
+function amountVowels() {
+  for (var char of string.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+console.log(count);
