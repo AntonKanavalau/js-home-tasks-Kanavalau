3d7d47f18e58bb2e650144bba0099097269acf33
Anton Kanavalau <old.projecta@gmail.com>
2022-11-24 13:46:20 +0300

    Пытался поработать с приватом, но фиаско

--- lesson-7-oop/app.js
+++ lesson-7-oop/app.js
@@ -2,6 +2,7 @@
 //ans - variants answer
 //trueAns - true answer
 
+//PUBLICK
 var Question = function (q, trueAns) {
   this.task = 'Complete the time expressions with IN, AT or ON';
   this.q = q;
@@ -49,6 +50,103 @@ function randomQuestion(allQuestions) {
 }
 
 function checkTrueAns(obj, custAnswer) {
+  if (obj.ans.findIndex(item => item === obj.trueAns) === custAnswer - 1 && custAnswer < 4) {
+    alert('Answer is True.');
+    console.log('Answer is True.');
+  } else if (isNaN(custAnswer) || custAnswer === '') {
+    alert('Please, you must select an answer.');
+    console.log('Please, you must select an answer.');
+    location.reload();
+  } else if (custAnswer > 3) {
+    alert('Sorry, you must choose answers from the 1 to 3.');
+    console.log('Sorry, you must choose answers from the 1 to 3.');
+    location.reload();
+  }else {
+    alert((`Answer is False. Your answer was ${custAnswer}-${obj.ans.find(item => item === obj.ans[custAnswer - 1])}.\nTrue Answer is ${(obj.ans.findIndex(item => item === obj.trueAns)) + 1}-${(obj.ans.find(item => item === obj.trueAns))}.`));
+    console.log(`Answer is False. Your answer was ${custAnswer}-${obj.ans.find(item => item === obj.ans[custAnswer - 1])}.\nTrue Answer is ${(obj.ans.findIndex(item => item === obj.trueAns)) + 1}-${(obj.ans.find(item => item === obj.trueAns))}.`);
+  }
+}
+
+//PRIVATE
+/*
+Хммм, пока не осилю... В массив мы добавляем объекты, но его переменные приватные...
+Как достучаться до них...
+
+function Question() {
+  var self = this;
+  task = 'Complete the time expressions with IN, AT or ON';
+  var q;
+  var ans = ['in', 'at', 'on'];
+  var trueAns = trueAns;
+
+  self.SetQuestion = function (_q) {
+    q = _q;
+  };
+
+  self.SetTrueAns = function (_trueAns) {
+    trueAns = _trueAns;
+  }
+
+  Question.prototype.numberingAns = function () {
+    var str = '';
+    for (var i = 0; i < ans.length; i++) {
+      str += `${i + 1}. ${ans[i]} \n`;
+    }
+    return str;
+  }
+
+  randomQuestion(allQuestions);
+  function randomQuestion() {
+    var randKey = Math.floor(Math.random() * allQuestions.length);
+    for (var i = 0; i < allQuestions.length; i++) {
+      if (randKey === i) {
+        obj = allQuestions[randKey];
+      }
+    }
+  }
+}
+
+
+var allQuestions = [];
+
+var q1 = new Question();
+q1.SetQuestion('__ Saturday');
+q1.SetTrueAns('on');
+allQuestions.push(q1);
+
+var q2 = new Question();
+q2.SetQuestion('__ summer');
+q2.SetTrueAns('in');
+allQuestions.push(q2);
+
+var q3 = new Question();
+q3.SetQuestion('__ the weekend');
+q3.SetTrueAns('at');
+allQuestions.push(q3);
+
+var q4 = new Question();
+q4.SetQuestion('__ the twenty-first century');
+q4.SetTrueAns('in');
+allQuestions.push(q4);
+
+var q5 = new Question();
+q5.SetQuestion('__ 2010');
+q5.SetTrueAns('in');
+allQuestions.push(q5);
+
+var q6 = new Question();
+q5.SetQuestion('__ eight o\'clock');
+q5.SetTrueAns('at');
+allQuestions.push(q6);
+
+//randomQuestion(allQuestions);
+
+//var custAnswer = parseInt(prompt(task + '\n' + '\n' + obj.SetQuestion() + '\n' + obj.numberingAns()));
+
+//checkTrueAns(obj, custAnswer);
+
+
+function checkTrueAns(obj, custAnswer) {
   if (obj.ans.findIndex(item => item === obj.trueAns) === custAnswer - 1) {
     alert('Answer is True.');
     console.log('Answer is True.');
@@ -61,3 +159,4 @@ function checkTrueAns(obj, custAnswer) {
     console.log(`Answer is False. Your answer was ${custAnswer}-${obj.ans.find(item => item === obj.ans[custAnswer - 1])}.\nTrue Answer is ${(obj.ans.findIndex(item => item === obj.trueAns)) + 1}-${(obj.ans.find(item => item === obj.trueAns))}.`);
   }
 }
+*/
