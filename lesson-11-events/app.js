e07ef26956cb4844b24753f0caf64757e8fe0845
Anton Kanavalau <old.projecta@gmail.com>
2022-12-08 17:23:05 +0300

    Finish

--- lesson-11-events/app.js
+++ lesson-11-events/app.js
@@ -23,6 +23,7 @@ function addIMGStyle(char) {
     char[i].style.position = 'absolute';
     char[i].style.left = `${char[i].width * i}px`;
     char[i].style.top = '0px';
+    //Кидаем слушателя на нажатие по картинке и вызываем функцию Drag_Start
     char[i].addEventListener('mousedown', Drag_Start, false);
   }
 }
@@ -30,20 +31,18 @@ function addIMGStyle(char) {
 function Drag_Start(EO) {
   EO = EO || window.event;
   EO.preventDefault();
-  console.log('Нажал');
 
   //Записали в глоб.переменную кликнутый элемент
   DragImage = EO.target;
-  DragImage.style.zIndex = '1';
-
-  //Шаманим с курсором
-  document.body.style.cursor = 'drop';
 
   //Записали в глоб.переменные координаты кликнутого элемента
   DragShiftX = EO.pageX - DragImage.offsetLeft;
   DragShiftY = EO.pageY - DragImage.offsetTop;
 
+  //Последняя нажатая картинка будет поверх остальных
   divContainer.appendChild(DragImage);
+
+  //Кидаем слушателя на ведение мыши в окне браузера
   window.addEventListener('mousemove', Drag_Move, false);
 }
 
@@ -51,22 +50,20 @@ function Drag_Start(EO) {
 function Drag_Move(EO) {
   EO = EO || window.event;
   EO.preventDefault();
-  console.log('Тащу');
 
   DragImage.style.left = (EO.pageX - DragShiftX) + 'px';
   DragImage.style.top = (EO.pageY - DragShiftY) + 'px';
 
+  //Кидаем слушателя на ведение мыши в окне браузера
   window.addEventListener('mouseup', Drag_Stop, false);
 }
 
+//Все обнулили
 function Drag_Stop(EO) {
   EO.preventDefault();
-  console.log('Отпустил');
 
   window.removeEventListener('mousemove', Drag_Move, false);
-
-  DragImage.style.zIndex = null;
-  document.body.style.cursor = 'default';
+  window.removeEventListener('mouseup', Drag_Stop, false);
 
   DragImage = null;
   DragShiftX = 0;
