3864914b418e95f98d242245370f700a30bc1d44
Anton Kanavalau <old.projecta@gmail.com>
2022-12-21 21:04:06 +0300

    Finish

--- lesson-13-timers-svg-canvas/Canvas_app.js
+++ lesson-13-timers-svg-canvas/Canvas_app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var radius = 700; //Размер canvas поля
-var center = `${radius / 2}`;//Центр canvas поля
+var center = radius / 2;//Центр canvas поля
 
 document.body.style.margin = '0px';
 
@@ -18,77 +18,81 @@ document.body.insertAdjacentElement('afterbegin', canvasBlock);
 var Canvas = document.getElementById('Canvas');
 var context = Canvas.getContext('2d');
 
+context.translate(center, center);
 
-//watch background 1
-context.beginPath();
-context.arc(`${center}`, `${center}`, `${center}`, 0, Math.PI * 2, false);
-context.fillStyle = '#FFCCEE';
-context.fill();
-context.closePath();
-
-//watch background 2
-context.save();
-context.beginPath();
-context.arc(`${center}`, `${center}`, `${center / 2}`, 0, Math.PI * 2, false);
-context.filter = 'blur(15px)';
-context.fillStyle = 'rgb(223 254 255 / 22%)';
-context.fill();
-context.closePath();
-context.restore();
-
-drawBGNumbers();
-drawNumbers();
-
-//create BG numbers
-function drawBGNumbers() {
-  for (var i = 0; i < 12; i++) {
-    context.translate(`${center}`, `${center}`);
-    context.rotate(30 * Math.PI / 180);
-    context.translate(`${-center}`, `${-center}`);
-    context.fillStyle = 'rgb(78 78 78 / 40%)';
-    context.beginPath();
-    context.arc(`${center}`, `${center / 5}`, `${center / 7}`, 0, Math.PI * 2, false);
-    context.fill();
-  }
+setInterval(drawClock, 1000);
+function drawClock() {
+  drawFace();
+  drawNumbers();
+  drawTime()
 }
 
+function drawFace() {
+//watch background 1
+  context.beginPath();
+  context.arc(0, 0, `${center}`, 0, Math.PI * 2, false);
+  context.fillStyle = '#FFCCEE';
+  context.fill();
+  context.closePath();
+
+//watch background 2
+  context.save();
+  context.beginPath();
+  context.arc(0, 0, `${center / 2}`, 0, Math.PI * 2, false);
+  context.filter = 'blur(15px)';
+  context.fillStyle = 'rgb(223 254 255 / 22%)';
+  context.fill();
+  context.closePath();
+  context.restore();
+
+  //center Dot
+  context.save();
+  context.beginPath();
+  context.arc(0, 0, `${center / 15}`, 0, Math.PI * 2, false);
+  context.fillStyle = 'black';
+  context.fill();
+  context.closePath();
+  context.restore();
+
+  context.fillStyle='#FFEEA9';
+  context.fillRect(center-(center/1.5)*2,center-(center/1.25),center/1.5, center/4);
+}
+
+//create numbers
 function drawNumbers() {
   context.textBaseline = 'middle';
   context.textAlign = 'center';
   context.font = `bold ${center / 7.4}px Arial`;
-  context.fillStyle = 'white';
 
   for (var i = 0; i < 12; i++) {
     var angle = (30 * (i + 1)) * Math.PI / 180;
     context.rotate(angle);
     context.translate(0, -center * 0.8);
     context.rotate(-angle);
-    context.fillText(`${i + 1}`, `${center}`, `${center}`);
+
+    context.fillStyle = 'rgb(78 78 78 / 40%)';
+    context.beginPath();
+    context.arc(0, 0, `${center / 7}`, 0, Math.PI * 2, false);
+    context.fill();
+
+    context.fillStyle = 'white';
+    context.fillText(`${i + 1}`, 0, 0);
+
     context.rotate(angle);
     context.translate(0, center * 0.8);
     context.rotate(-angle);
   }
 }
 
-//center Dot
-context.save();
-context.beginPath();
-context.arc(`${center}`, `${center}`, `${center / 15}`, 0, Math.PI * 2, false);
-context.fillStyle = 'black';
-context.fill();
-context.closePath();
-context.restore();
-
 //Работаем с временем
-setInterval(drawTime, 1000);
-
-context.translate(center, center);
-
 function drawTime() {
   let CurrTime = new Date();
   let hour = CurrTime.getHours();
   let minute = CurrTime.getMinutes();
   let second = CurrTime.getSeconds();
+
+  context.fillStyle = 'red';
+  context.fillText(Str0L(hour, 2) + ':' + Str0L(minute, 2) + ':' + Str0L(second, 2), 0, center-(center/1.5));
 
 // часовая стрелка
   hour = hour % 12;
@@ -100,6 +104,13 @@ function drawTime() {
 // секундная
   second = (second * Math.PI / 30);
   drawHand(context, second, center * 0.9, center * 0.02);
+
+  function Str0L(Val, Len) {
+    var StrVal = Val.toString();
+    while (StrVal.length < Len)
+      StrVal = '0' + StrVal;
+    return StrVal;
+  }
 }
 
 function drawHand(context, angle, length, width) {
