7ade7639b2d64b586bf20e8f070457b6c8a516c7
Anton Kanavalau <old.projecta@gmail.com>
2023-01-15 13:53:26 +0300

    finish task

---
+++ lesson-19-mvc/LocalStorage.js
@@ -0,0 +1,29 @@
+function TLocalStorage(keyName) {
+  var self = this,
+    pHash = {};
+
+  self.addValue = function (key, value) {
+    pHash[key] = value;
+    localStorage.setItem(keyName, JSON.stringify(pHash));
+  };
+
+  self.getValue = function (key) {
+    return pHash[key];
+  };
+
+  self.deleteValue = function (key) {
+    delete pHash[key];
+    localStorage.setItem(keyName, JSON.stringify(pHash));
+    return delete pHash[key];
+  };
+
+  self.getKeys = function () {
+    return (Object.keys(pHash));
+  };
+
+  self.reset = function () {
+    pHash = JSON.parse(localStorage.getItem(keyName));
+  }
+  
+  self.reset();
+}
